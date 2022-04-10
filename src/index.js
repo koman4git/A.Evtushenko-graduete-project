e44301badd64c4c0698514d52b90ef64bc43e98d
Andrey Evtushenko <koman4git@gmail.com>
2022-04-10 14:54:00 +0300

    добавлен функционал для кнопки с классом .button-services

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import modal from './modules/modal'
 import servisesScroll from './modules/servises'
 import slider from './modules/slider'
 import smoothScroll from './modules/smoothScroll'
+import btnServises from './modules/btnServ'
 
 
 modal()
 servisesScroll()
 slider()
 smoothScroll()
+btnServises()
---
+++ src/modules/btnServ.js
@@ -0,0 +1,53 @@
+'use strict';
+import { animate } from "./helpers"
+
+const btnServises = () => {
+    const btnServ = document.querySelector('.button-services')
+    const modalCallback = document.querySelector('.modal-callback')
+    const modalOverlay = document.querySelector('.modal-overlay')
+    const modalClose = document.querySelector('.modal-close')
+  
+    const screen = window.screen.width
+  
+    btnServ.addEventListener('click', () => {
+        modalOverlay.style.display = 'block'
+        modalCallback.style.display = 'block'
+        modalCallback.style.top = -50 + '%'
+  
+        if (screen > 768) {
+          animate({
+            duration: 500,
+            timing(timeFraction) {
+              return timeFraction
+            },
+            draw(progress) {
+              modalCallback.style.top = -50 + 80 * progress + '%'
+            },
+          })
+        } else {
+          modalOverlay.style.display = 'block'
+          modalCallback.style.top = 20 + '%'
+        }
+      })
+    
+  
+    modalOverlay.addEventListener('click', () => {
+      modalOverlay.style.display = 'none'
+      modalCallback.style.display = 'none'
+    })
+  
+    modalClose.addEventListener('click', () => {
+      modalOverlay.style.display = 'none'
+      modalCallback.style.display = 'none'
+    })
+  
+    document.addEventListener('keydown', (e) => {
+      const key = e.key
+      if (key === 'Escape') {
+        modalOverlay.style.display = 'none'
+        modalCallback.style.display = 'none'
+      }
+    })
+  }
+
+export default btnServises
